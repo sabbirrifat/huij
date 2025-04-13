0ae97457570e09f6e7a20d704632b278920c27f2
Sabbir Rifat <rifat234dgh@gmail.com>
2025-04-14 05:06:20 +0600

    updated

---
+++ njnf.js
@@ -0,0 +1,78 @@
+//size 2.8 million
+const AccomodationServices = {};
+
+//size 1.6 million
+const AdministrativeAndSupportServices = {};
+
+//size 400k
+const Construction = {};
+
+//size 75k only
+const ConsumerGoods = {};
+
+//size 1.5 million
+const ConsumerServices = {};
+
+//size 5.4 million
+const Education = {};
+
+//size 1.4 million
+const EntertainmnetProviders = {};
+
+//size 122k only
+const FarmingRanchingAndForestry = {};
+
+// size 2.7 million
+const FinancialServices = {};
+
+//size 122k only
+const FoodProduction = {};
+
+//size 2.7 million
+
+const GovermentAdministration = {};
+
+//size 247k only
+const HealthCare = {};
+
+//size 1.5 million
+const hospitalsAndHealthCare = {};
+
+// size 7.1 million
+
+const Manufacturing = {};
+
+// size 7k only
+
+const NonProfitOrganizations = {};
+
+// size 800k
+
+const OilGasAndMining = {};
+
+//size 7.5 million
+
+const ProfessionalServices = {};
+
+// size 1.8 million
+const RealEstateAndEquipmentRentalServices = {};
+
+// size 820k
+
+const Retail = [];
+
+// size 4.6 million
+
+const TechnologyInformationAndMedia = [];
+
+//size 1.5 million
+
+const TransportationLogisticsAndSupplyChain = {};
+
+// size 92k only
+
+const Utilities = {};
+
+// size 355k
+
+const Wholesale = {};
